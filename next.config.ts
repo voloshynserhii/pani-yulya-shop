76f84fcc09d6d706df05154458ad1be9f3996681
Serhii Voloshyn <vosquery@gmail.com>
2026-01-22 15:49:54 +0100

    try

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,16 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/@swc/core-linux-x64-gnu',
+        'node_modules/@swc/core-linux-x64-musl',
+        'node_modules/@esbuild/linux-x64',
+        'node_modules/webpack',
+      ],
+    },
+  },
 };
 
 export default nextConfig;
